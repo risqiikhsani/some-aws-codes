2969b41b422dcadfb5c59ee2822c034855d28830
Risqi Ikhsani <48210802+risqiikhsani@users.noreply.github.com>
2024-06-04 13:57:31 +0700

    Update bedrock.py

--- lambda/bedrock.py
+++ lambda/bedrock.py
@@ -1,3 +1,5 @@
+
+
 import json
 import boto3
 
@@ -17,7 +19,6 @@ def invoke_model(body, model_id, accept, content_type):
     Returns:
         Inference response from the model.
     """
-
     try:
         response = bedrock_runtime.invoke_model(
             body=json.dumps(body), 
@@ -25,31 +26,24 @@ def invoke_model(body, model_id, accept, content_type):
             accept=accept, 
             contentType=content_type
         )
-
         return response
-
     except Exception as e:
+        print(f"Error: {e}")
         print(f"Couldn't invoke {model_id}")
         raise e
 
-
+# note, increase timeout lambda to long time in order to work (15 minutes)
 # https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-anthropic-claude-messages.html
 def lambda_handler(event, context):
+    # Extract the request body from the event
     body = json.loads(event["body"])
     messages = body["messages"]
+    print(f"received messages {messages}")
     
-    # messages
-    # [
-    #   {"role": "user", "content": "Hello there."},
-    #   {"role": "assistant", "content": "Hi, I'm Claude. How can I help you?"},
-    #   {"role": "user", "content": "Can you explain LLMs in plain English?"},
-    # ]
+    system_prompt = "You are BOT from website called Health4Us an AI assistant to be helpful,harmless, and honest about healthcare. Your goal is to provide informative and substantive responses to queries related to healthcare only , while avoiding potential harms. example , if user provides a symptoms , you will answer the most likely cause or disease name and how to prevent it. if user provides a disease name , you can answer about the symptoms and how to prevent it, etc. "
+    max_tokens = 1000
     
-    system_prompt = "You are BOT from website called Health4Us, an AI assistant to be helpful,harmless, and honest about healthcare. Your goal is to provide informative and substantive responses to queries related to healthcare only , while avoiding potential harms. example , if user provides a symptoms , you will answer the most likely cause or disease name and how to prevent it. if user provides a disease name , you can answer about the symptoms and how to prevent it, etc. "
-    max_tokens=1000
-    # user_message =  {"role": "user", "content": text}
-    # messages = [user_message]
-    body={
+    body = {
         "anthropic_version": "bedrock-2023-05-31",
         "max_tokens": max_tokens,
         "system": system_prompt,
@@ -59,15 +53,26 @@ def lambda_handler(event, context):
     modelId = "anthropic.claude-3-haiku-20240307-v1:0"  # change this to use a different version from the model provider
     accept = "application/json"
     contentType = "application/json"
+    try:
+        response = invoke_model(body, modelId, accept, contentType)
+        print(response)
+        response_body = json.loads(response.get("body").read())
+        print(response_body.get("content"))
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response_body.get("content"))
+        }
+    except Exception as e:
+        print(f"Error: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Internal Server Error')
+        }
     
-    response = invoke_model(body, modelId, accept, contentType)
-    print(response)
-    response_body = json.loads(response.get("body").read())
-    print(response_body.get("content"))
-    # print(response_body.get("completion"))
-    
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response_body.get("content"))
-    }
+    # messages
+    # [
+    #   {"role": "user", "content": "Hello there."},
+    #   {"role": "assistant", "content": "Hi, I'm Claude. How can I help you?"},
+    #   {"role": "user", "content": "Can you explain LLMs in plain English?"},
+    # ]
